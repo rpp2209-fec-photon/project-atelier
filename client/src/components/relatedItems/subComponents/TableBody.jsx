0727ecadee44d57ead41f785b77bc650de690593
Thanh Ly <thanhgly.se@gmail.com>
2023-02-06 22:48:03 -0800

    ComparisonModal and TableBody components done

---
+++ client/src/components/relatedItems/subComponents/TableBody.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { TiTick } from 'react-icons/ti';
+
+const TableBody = (props) => {
+
+  const {cardMetadata, productMetadata} = props;
+
+  const cardChars = cardMetadata ? cardMetadata.characteristics ? cardMetadata.characteristics : {} : {};
+  const productChars = productMetadata ? productMetadata.characteristics ? productMetadata.characteristics : {} : {};
+
+  const characteristics = new Set([...Object.keys(cardChars), ...Object.keys(productChars)]);
+
+  return (
+  <tbody>
+
+    {
+      Array.from(characteristics).map((char, index) => {
+        return <tr key={index}>
+          <td className='left'> {cardChars[char] ? <TiTick/> : null} </td>
+          <td className='middle'> {char} </td>
+          <td className='right'> {productChars[char] ? <TiTick/> : null} </td>
+        </tr>
+      })
+    }
+
+  </tbody>
+  )
+};
+
+export default TableBody;
