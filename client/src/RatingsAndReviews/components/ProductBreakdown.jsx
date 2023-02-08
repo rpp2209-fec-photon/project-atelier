db02a239e4ea80444da7ac805de620110dc7336e
Oscar Sandoval <oscarjs707@gmail.com>
2023-02-08 12:27:47 -0800

    Showing characteristic names

---
+++ client/src/RatingsAndReviews/components/ProductBreakdown.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+import CharacterBreakdown from './miniComponents/CharacterBreakdown.jsx';
+
+import {getMetaReviews} from '../../../helpers/helpers.js';
+
+export default function ProductBreakdown ({productID}){
+
+  var [characteristics, setCharacteristics] = useState({});
+
+
+  useEffect(()=>{
+    getMetaReviews(productID)
+    .then((data)=>{
+      setCharacteristics(data.data.characteristics);
+    });
+  }, []);
+
+  return (
+    <div>
+      {Object.keys(characteristics).map((key)=>{
+        return (
+          <CharacterBreakdown character={key} id={characteristics[key].id} value={characteristics[key].value}/>
+        );
+      })}
+    </div>
+  );
+};
---
+++ client/src/RatingsAndReviews/components/miniComponents/CharacterBreakdown.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CharacterBreakdown ({character, id, value}){
+  return (
+    <div>
+      <h3>{character}</h3>
+    </div>
+  );
+};
