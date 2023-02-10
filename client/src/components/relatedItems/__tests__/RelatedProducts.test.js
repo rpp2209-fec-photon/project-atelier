49325f2fe45fb492be32704baa2c1c2f011ca519
Thanh Ly <thanhgly.se@gmail.com>
2023-02-09 23:02:51 -0800

    minor changes to RelatedProducts tests

--- client/src/components/relatedItems/__tests__/RelatedProducts.test.js
+++ client/src/components/relatedItems/__tests__/RelatedProducts.test.js
@@ -2,27 +2,22 @@
 * @jest-environment jsdom
 */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {rest} from 'msw';
 import {setupServer} from 'msw/node';
+import {handlers} from './handlers.js';
 
 import {render, fireEvent, waitFor, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import RelatedProducts from '../RelatedProducts.jsx';
-import Card from '../Card.jsx';
 
-const server = setupServer(
-  rest.get('http://localhost:3000/products/12345/related/', (req, res, ctx) => {
-    return res(ctx.json([1, 2, 3, 4, 5]));
-  }),
-);
+const server = setupServer(...handlers);
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
-test('Card component', async () => {
-  render(<RelatedProducts productId={12345} />);
-  expect(screen.getByRole('heading')).toHaveTextContent('Related Products');
+test('RelatedProducts component', async () => {
+
 });
