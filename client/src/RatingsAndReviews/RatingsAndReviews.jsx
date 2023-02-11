9e7872da81f27782ed80f5c4428d58e258c27c5f
Oscar Sandoval <oscarjs707@gmail.com>
2023-02-10 19:27:08 -0800

    Added sorting functionality

--- client/src/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/src/RatingsAndReviews/RatingsAndReviews.jsx
@@ -11,16 +11,18 @@ import helpers from '../../helpers/helpers.js';
 
 export default function RatingsAndReviews ({productID, productName}) {
 
+  var [sort, setSort] = useState('newest');
 
   var [productReviews, setProductReviews] = useState({results:[]});
   var [reviewsShown, setReviewsShown] = useState(2);
   var [newReviewVisibility, setNewReviewVisibility] = useState('hidden');
   var [characteristics, setCharacteristics] = useState({});
+
   var page = useRef(1);
 
   useEffect(()=>{
     console.log(productID);
-    helpers.getReviews(page.current, 6, 'newest', productID)
+    helpers.getReviews(page.current, 6, sort, productID)
     .then((reviews)=>{
       setProductReviews({...reviews.data});
     }).then(()=>{
@@ -29,14 +31,14 @@ export default function RatingsAndReviews ({productID, productName}) {
         setCharacteristics(data.data.characteristics);
       });
     });
-  }, [productID]);
+  }, [productID, sort]);
 
   var showMoreReviews = ()=>{
     if (reviewsShown % 6 !== 0) {
       setReviewsShown(reviewsShown + 2);
     } else if (reviewsShown % 6 === 0) {
       page.current ++;
-      helpers.getReviews(page.current, 6, 'newest', productID)
+      helpers.getReviews(page.current, 6, sort, productID)
       .then((reviews)=>{
         setProductReviews({...productReviews, results: productReviews.results.concat(reviews.data.results)});
         setReviewsShown(reviewsShown + 2);
@@ -52,13 +54,14 @@ export default function RatingsAndReviews ({productID, productName}) {
         <RatingBreakdown productID={productID}/>
         <ProductBreakdown productID={productID} characteristics={characteristics}/>
       </div>
+      <SortReviews setSort={setSort}/>
     <div className="ReviewList">
 
       {
         productReviews.results.map((review, index)=>{
 
           if (index < reviewsShown) {
-
+            console.log(review);
             return (
               <div className="ReviewTile" key={index}>
                 <ReviewTile Review={review} key={index} productID={productID}/>
