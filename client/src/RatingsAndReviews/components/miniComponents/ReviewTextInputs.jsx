40f1eadfd85ec6f51740c5c6a6c978b87df9ff9c
Oscar Sandoval <oscarjs707@gmail.com>
2023-02-10 17:06:31 -0800

    Added feature that makes an error message pops up if mandatory fields are empty.

--- client/src/RatingsAndReviews/components/miniComponents/ReviewTextInputs.jsx
+++ client/src/RatingsAndReviews/components/miniComponents/ReviewTextInputs.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import {useState} from 'react';
 
-export default function ReviewTextInputs (){
+export default function ReviewTextInputs ({reviewSummary, setReviewSummary, reviewBody, setReviewBody}){
+
+  var [usedChars, setUsedChars] = useState(0);
+
   return (
     <>
-      <TextInput maxChar={60} placeHolder={'Example: Best purchase ever!'}/>
-      <TextInput maxChar={1000} placeHolder={'Why did you like the product or not?'}/>
+      <TextInput maxChar={60} placeHolder={'Example: Best purchase ever!'}  handleChange={(e)=>{setReviewSummary(e.target.value)}}/>
+      <TextInput maxChar={1000} placeHolder={'Why did you like the product or not?'} handleChange={(e)=>{setUsedChars(e.target.value.length);setReviewBody(e.target.value)}}/>
+      <ShowUsedChars usedChars={usedChars}/>
     </>
   );
 };
 
 
-var TextInput = ({maxChar, placeHolder})=>{
+var TextInput = ({maxChar, placeHolder, handleChange})=>{
 
   return (
-    <textarea className='TextInput' rows="10" cols="30" maxLength={maxChar}>{placeHolder}</textarea>
+    <textarea onChange={handleChange}className='TextInput' rows="10" cols="30" maxLength={maxChar} defaultValue={placeHolder}></textarea>
   );
+};
+
+var ShowUsedChars = ({usedChars})=>{
+  if (usedChars >= 50) {
+    return (<p>Minimum reached</p>)
+  } else {
+    return (
+      <p>Minimum requires characters left: {50 - usedChars}.</p>
+    );
+  }
 };
