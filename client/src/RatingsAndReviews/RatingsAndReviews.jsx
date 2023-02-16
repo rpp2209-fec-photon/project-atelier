95114b5bad335a9839f3a612320bc6105c547eca
Oscar Sandoval <oscarjs707@gmail.com>
2023-02-15 16:57:06 -0800

    Improved filtering

--- client/src/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/src/RatingsAndReviews/RatingsAndReviews.jsx
@@ -10,6 +10,7 @@ import ImageZoom from './components/miniComponents/ImageZoom.jsx';
 
 
 import helpers from '../../helpers/helpers.js';
+import {getTotalReviews} from './compHelpers.js';
 
 
 export default function RatingsAndReviews ({productID, productName}) {
@@ -18,39 +19,61 @@ export default function RatingsAndReviews ({productID, productName}) {
   var [ratingFilter, setRatingFilter] = useState([]);
 
   var [productReviews, setProductReviews] = useState({results:[]});
+  var totalReviews = useRef(0);
   var [newReviewVisibility, setNewReviewVisibility] = useState('hidden');
   var [ImageZoomVisibility, setImageZoomVisibility] = useState('hidden');
   var [imageURL, setImageURL] = useState('');
   var [characteristics, setCharacteristics] = useState({});
-  var page = useRef(1);
   var [reviewsShown, setReviewsShown] = useState(2);
 
   useEffect(()=>{
-    //get the first 6 reviews
-    helpers.getReviews(1, 6, sort, productID)
-    .then((reviews)=>{
-      setProductReviews({...reviews.data});
-      setReviewsShown(2);
-    }).then(()=>{
-      //get meta data of reviews
-      helpers.getMetaReviews(productID)
-      .then((data)=>{
-        setCharacteristics(data.data.characteristics);
+    //get meta data of reviews
+    helpers.getMetaReviews(productID)
+    .then((data)=>{
+      setCharacteristics(data.data.characteristics);
+
+      totalReviews.current = getTotalReviews(data.data.ratings);
+
+      //get all the reviews
+      helpers.getReviews(1, totalReviews.current, sort, productID)
+      .then((reviews)=>{
+        if (ratingFilter.length > 0) {
+          setProductReviews({...reviews.data, results: filterReviews(reviews.data)});
+        } else {
+          setProductReviews({...reviews.data});
+
+        }
+
+        setReviewsShown(2);
       });
-    });
-  }, [productID, sort]);
+    })
+
+  }, [productID, sort, ratingFilter]);
+
+  var filterReviews = (reviews)=>{
+
+    var filteredReviews = [];
+
+    console.log(ratingFilter);
+
+    //loop through the reviews
+    for (var x = 0; x < reviews.results.length; x++) {
+      //check if the review passes through the filter
+      for (var i = 0; i < ratingFilter.length; i++) {
+        if (reviews.results[x].rating === ratingFilter[i]) {
+          //push the review to the filteredReviews array
+          filteredReviews.push(reviews.results[x]);
+        }
+      }
+    }
+    return filteredReviews;
+  };
+
 
   var showMoreReviews = (increment)=>{
 
     setReviewsShown(reviewsShown+increment);
 
-    if (reviewsShown % 6 === 0 ) {
-      console.log('running');
-      helpers.getReviews(1, productReviews.results.length + increment, sort, productID)
-      .then((reviews)=>{
-        setProductReviews({...reviews.data});
-      });
-    }
   };
 
 
